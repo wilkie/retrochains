9ce7c8e838ca3f52f2bc462d07277e9134521e90
wilkie <wilkie05@gmail.com>
2026-05-18 09:39:46 -0700

    Adds fixtures for stack-LHS ulong DIV with unsigned int and signed long ADD/MUL with unsigned int.

---
+++ fixtures/779-ulong-local-compound-div-uint-var-obj/HELLO.C
@@ -0,0 +1,8 @@
+int main() {
+  unsigned long a;
+  unsigned int x;
+  a = 100;
+  x = 5;
+  a /= x;
+  return 0;
+}
---
+++ fixtures/781-long-global-compound-mul-uint-var-obj/HELLO.C
@@ -0,0 +1,8 @@
+long g;
+int main() {
+  unsigned int x;
+  g = 100;
+  x = 50;
+  g *= x;
+  return 0;
+}
