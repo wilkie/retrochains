8ae3648fd3d3f8995577d209f16f2fe4d37e95df
wilkie <wilkie05@gmail.com>
2026-05-20 14:15:27 -0700

    Adds fixtures: (int)(long+long) skips high adc, long arg cdecl high-pushed-first, long cmp int const promotes at parse time.

---
+++ fixtures/1646-arr-of-long-obj/HELLO.C
@@ -0,0 +1,6 @@
+int main(void) {
+  long a[2];
+  a[0] = 100L;
+  a[1] = 200L;
+  return (int)(a[0] + a[1]);
+}
---
+++ fixtures/1647-fn-long-arg-obj/HELLO.C
@@ -0,0 +1,7 @@
+long sqr(long x) {
+  return x * x;
+}
+int main(void) {
+  long r = sqr(7L);
+  return (int)r;
+}
