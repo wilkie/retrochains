aeb0ea74bbbda482dbb4962bd934aca865671dce
wilkie <wilkie05@gmail.com>
2026-05-20 10:38:43 -0700

    Adds fixtures for int := a[0]+a[2] folded offsets, int := (cmp||cmp) short-circuit, a[1] = v+2 inc-pair.

---
+++ fixtures/1481-int-arr-elem-add-elem-obj/HELLO.C
@@ -0,0 +1,5 @@
+int main(void) {
+  int a[3] = {10, 20, 30};
+  int x = a[0] + a[2];
+  return x;
+}
---
+++ fixtures/1482-int-init-from-or-cmps-obj/HELLO.C
@@ -0,0 +1,6 @@
+int main(void) {
+  int a = 0;
+  int b = 5;
+  int x = (a > 0) || (b > 0);
+  return x;
+}
