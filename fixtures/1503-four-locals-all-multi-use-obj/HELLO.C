fa6a638431b9d56b754a1e921cb813f46b67b2cc
wilkie <wilkie05@gmail.com>
2026-05-20 11:02:48 -0700

    Adds fixtures showing BX joins SI/DI/DX as enregistration target when 4 multi-use ints competing.

---
+++ fixtures/1503-four-locals-all-multi-use-obj/HELLO.C
@@ -0,0 +1,11 @@
+int main(void) {
+  int a = 1;
+  int b = 2;
+  int c = 3;
+  int d = 4;
+  a = a + 1;
+  b = b + 1;
+  c = c + 1;
+  d = d + 1;
+  return a + b + c + d;
+}
---
+++ fixtures/1504-one-local-used-many-obj/HELLO.C
@@ -0,0 +1,7 @@
+int main(void) {
+  int v = 1;
+  v = v + 1;
+  v = v + 2;
+  v = v + 3;
+  return v;
+}
