78945a6f27380a893fb17cab3a8beb5ea82592a4
wilkie <wilkie05@gmail.com>
2026-05-19 19:26:54 -0700

    Adds fixtures for AND short-circuit side effect, fn-takes-char-array, comma in for-init.

---
+++ fixtures/1239-fn-takes-char-array-obj/HELLO.C
@@ -0,0 +1,7 @@
+char b[2] = {3, 4};
+int sum(char a[]) {
+  return a[0] + a[1];
+}
+int main(void) {
+  return sum(b);
+}
---
+++ fixtures/1240-for-comma-init-obj/HELLO.C
@@ -0,0 +1,6 @@
+int main(void) {
+  int i;
+  int s;
+  for (i = 0, s = 10; i < 3; i++) s += i;
+  return s;
+}
