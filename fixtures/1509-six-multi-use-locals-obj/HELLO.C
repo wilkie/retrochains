89a73afd311c771efa8a5fb0b7fc6375d142b4d1
wilkie <wilkie05@gmail.com>
2026-05-20 11:11:33 -0700

    Adds fixtures confirming call-crossing locals limited to SI/DI; 6th candidate spills to stack.

---
+++ fixtures/1509-six-multi-use-locals-obj/HELLO.C
@@ -0,0 +1,15 @@
+int main(void) {
+  int a = 1;
+  int b = 2;
+  int c = 3;
+  int d = 4;
+  int e = 5;
+  int f = 6;
+  a = a + 1;
+  b = b + 1;
+  c = c + 1;
+  d = d + 1;
+  e = e + 1;
+  f = f + 1;
+  return a + b + c + d + e + f;
+}
