4d78473740171b0d079ddab1692bd50e6af02e55
wilkie <wilkie05@gmail.com>
2026-05-19 23:20:23 -0700

    Adds fixtures for int += ternary, char arr elem += const, array subscript via call result.

---
+++ fixtures/1370-int-compound-add-ternary-obj/HELLO.C
@@ -0,0 +1,6 @@
+int main(void) {
+  int a = 5;
+  int b = 3;
+  a += b > 0 ? 10 : -10;
+  return a;
+}
---
+++ fixtures/1371-char-arr-elem-rmw-obj/HELLO.C
@@ -0,0 +1,7 @@
+int main(void) {
+  char a[3];
+  a[0] = 10;
+  a[1] = 20;
+  a[1] += 5;
+  return a[1];
+}
