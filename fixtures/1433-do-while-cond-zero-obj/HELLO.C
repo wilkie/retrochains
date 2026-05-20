5f3266c89af22571d96547e62d252dd8328a20b9
wilkie <wilkie05@gmail.com>
2026-05-20 02:29:46 -0700

    Adds fixtures for do-while with const-zero cond, if cond with assign expr, four-arm chained ternary.

---
+++ fixtures/1433-do-while-cond-zero-obj/HELLO.C
@@ -0,0 +1,7 @@
+int main(void) {
+  int n = 0;
+  do {
+    n++;
+  } while (0);
+  return n;
+}
---
+++ fixtures/1435-chained-ternary-obj/HELLO.C
@@ -0,0 +1,4 @@
+int main(void) {
+  int a = 2;
+  return a == 0 ? 100 : a == 1 ? 200 : a == 2 ? 300 : 0;
+}
