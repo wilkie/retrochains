e2321d4409d6dbe6aa6c0f072a9b6b9cd21c37c4
wilkie <wilkie05@gmail.com>
2026-05-20 12:58:46 -0700

    Adds fixtures showing do-while(1) / for(;;) / for(init;;incr) all canonicalise to byte-identical loop code as while(1).

---
+++ fixtures/1589-do-while-const-true-obj/HELLO.C
@@ -0,0 +1,8 @@
+int main(void) {
+  int i = 0;
+  do {
+    if (i == 3) break;
+    i++;
+  } while (1);
+  return i;
+}
---
+++ fixtures/1591-for-empty-all-obj/HELLO.C
@@ -0,0 +1,8 @@
+int main(void) {
+  int i = 0;
+  for (;;) {
+    if (i == 3) break;
+    i++;
+  }
+  return i;
+}
