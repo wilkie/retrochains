538cf1a8f2574154b3645e7a59a41ce404d89ec2
wilkie <wilkie05@gmail.com>
2026-05-19 18:30:41 -0700

    Adds fixtures for for-loop summing index, stack int-array sum, nested for-loop counter.

---
+++ fixtures/1206-stack-int-array-sum-obj/HELLO.C
@@ -0,0 +1,7 @@
+int main(void) {
+  int a[3];
+  a[0] = 1;
+  a[1] = 2;
+  a[2] = 3;
+  return a[0] + a[1] + a[2];
+}
---
+++ fixtures/1207-nested-for-loop-counter-obj/HELLO.C
@@ -0,0 +1,8 @@
+int main(void) {
+  int s = 0;
+  int i, j;
+  for (i = 0; i < 2; i++)
+    for (j = 0; j < 2; j++)
+      s++;
+  return s;
+}
