2497cd0d9d5b9bd25db6b010f7f96e09f0c1fd54
wilkie <wilkie05@gmail.com>
2026-05-20 13:47:21 -0700

    Adds fixtures: signed char*char promotes via cbw+push/pop; uchar*uchar uses mov ah=0 zero-extend; (char)(uint>>4) keeps word shr no propagation.

---
+++ fixtures/1626-uchar-times-uchar-into-int-obj/HELLO.C
@@ -0,0 +1,6 @@
+int main(void) {
+  unsigned char a = 200;
+  unsigned char b = 3;
+  int r = a * b;
+  return r;
+}
---
+++ fixtures/1627-cast-uint-shr-obj/HELLO.C
@@ -0,0 +1,4 @@
+int main(void) {
+  unsigned int v = 0x1234;
+  return (char)(v >> 4);
+}
