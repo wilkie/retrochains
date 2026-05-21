348dcbc9cf7e2e5f86e01cd57a2a58ad46f2327f
wilkie <wilkie05@gmail.com>
2026-05-20 23:06:56 -0700

    Adds fixtures: base=0 omits normalize; HUGE GAP uses 3rd strategy = linear-search through value table via 'loop' insn; negative base via sub bx,K imm8-sext.

---
+++ fixtures/1908-switch-4-cases-huge-gap-obj/HELLO.C
@@ -0,0 +1,11 @@
+int main(void) {
+  int x = 2;
+  int r = 0;
+  switch (x) {
+    case 1: r = 11; break;
+    case 2: r = 12; break;
+    case 3: r = 13; break;
+    case 50: r = 50; break;
+  }
+  return r;
+}
