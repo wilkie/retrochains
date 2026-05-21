e4a259cebf5ffe6f52b1808b20d1a7b0e06464de
wilkie <wilkie05@gmail.com>
2026-05-20 21:46:19 -0700

    Adds fixtures: && uses inverse-jcc chain to false target; || uses true-jcc then false-jcc; mixed cmp & && works as expected.

---
+++ fixtures/1856-short-circuit-and-obj/HELLO.C
@@ -0,0 +1,6 @@
+int main(void) {
+  int a = 5;
+  int b = 0;
+  if (a && b) return 1;
+  return 0;
+}
---
+++ fixtures/1857-short-circuit-or-obj/HELLO.C
@@ -0,0 +1,6 @@
+int main(void) {
+  int a = 0;
+  int b = 7;
+  if (a || b) return 1;
+  return 0;
+}
