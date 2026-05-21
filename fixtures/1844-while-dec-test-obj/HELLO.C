605653d52d6377866569875403d3bb3b2216218b
wilkie <wilkie05@gmail.com>
2026-05-20 21:29:28 -0700

    Adds fixtures: while(--n) combines dec+jne (3B); ==/!= inverse jcc; unsigned < uses jae for signedness-correct false-branch.

---
+++ fixtures/1844-while-dec-test-obj/HELLO.C
@@ -0,0 +1,8 @@
+int main(void) {
+  int n = 5;
+  int count = 0;
+  while (--n) {
+    count++;
+  }
+  return count;
+}
---
+++ fixtures/1846-cmp-uint-lt-obj/HELLO.C
@@ -0,0 +1,6 @@
+int main(void) {
+  unsigned int a = 0x8000;
+  unsigned int b = 0x0001;
+  if (a < b) return 1;
+  return 0;
+}
