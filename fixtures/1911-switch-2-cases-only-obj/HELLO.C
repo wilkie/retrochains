1cb834c903df2b8bc3fc38dc14a5ad7b83bbdbf0
wilkie <wilkie05@gmail.com>
2026-05-20 23:10:38 -0700

    Adds fixtures: 3+default = linear chain (default doesnt count); 2 cases linear; imm16 base uses 81 eb imm16 (4B).

---
+++ fixtures/1911-switch-2-cases-only-obj/HELLO.C
@@ -0,0 +1,9 @@
+int main(void) {
+  int x = 2;
+  int r = 0;
+  switch (x) {
+    case 1: r = 11; break;
+    case 2: r = 12; break;
+  }
+  return r;
+}
---
+++ fixtures/1912-switch-large-base-imm16-obj/HELLO.C
@@ -0,0 +1,11 @@
+int main(void) {
+  int x = 200;
+  int r = 0;
+  switch (x) {
+    case 200: r = 1; break;
+    case 201: r = 2; break;
+    case 202: r = 3; break;
+    case 203: r = 4; break;
+  }
+  return r;
+}
