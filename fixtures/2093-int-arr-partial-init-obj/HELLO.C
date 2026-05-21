e07b0b8b94c566b6f725594e2433d9df1849b20d
wilkie <wilkie05@gmail.com>
2026-05-21 07:41:11 -0700

    Adds fixtures: int arr[5]={1,2} zero-pads tail; int arr[]={...} sized to init count; char s[]="abc" sized to strlen+1 (includes null).

---
+++ fixtures/2093-int-arr-partial-init-obj/HELLO.C
@@ -0,0 +1,4 @@
+int main(void) {
+  static int arr[5] = {1, 2};
+  return arr[0] + arr[1] + arr[2] + arr[3] + arr[4];
+}
---
+++ fixtures/2094-int-arr-implicit-size-obj/HELLO.C
@@ -0,0 +1,4 @@
+int main(void) {
+  static int arr[] = {10, 20, 30, 40};
+  return arr[0] + arr[1] + arr[2] + arr[3];
+}
