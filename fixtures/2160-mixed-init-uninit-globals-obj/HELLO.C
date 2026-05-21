fc45c50eb40ab00ed05f93a28842171018522f62
wilkie <wilkie05@gmail.com>
2026-05-21 09:28:54 -0700

    Adds fixtures: 3 init globals source-order in _DATA but PUBDEF reverse; mixed init/uninit partitioned _DATA + _BSS; uninit arr in _BSS sized 10B.

---
+++ fixtures/2160-mixed-init-uninit-globals-obj/HELLO.C
@@ -0,0 +1,7 @@
+int a = 5;
+int b;
+int c = 10;
+int main(void) {
+  b = 7;
+  return a + b + c;
+}
---
+++ fixtures/2161-global-arr-uninit-bss-obj/HELLO.C
@@ -0,0 +1,6 @@
+int arr[5];
+int main(void) {
+  arr[0] = 1;
+  arr[4] = 5;
+  return arr[0] + arr[4];
+}
