21e3b0ce0f2a70d962cf10a7aeeb07b654319b2e
wilkie <wilkie05@gmail.com>
2026-05-20 22:51:37 -0700

    Adds fixtures: 8 contiguous cases USE JUMP TABLE via cs:jmp[bx+table] - overturns linear-chain-always claim; char switch promotes via cbw; static arr in _DATA.

---
+++ fixtures/1898-switch-8-cases-obj/HELLO.C
@@ -0,0 +1,15 @@
+int main(void) {
+  int x = 5;
+  int r = 0;
+  switch (x) {
+    case 1: r = 11; break;
+    case 2: r = 12; break;
+    case 3: r = 13; break;
+    case 4: r = 14; break;
+    case 5: r = 15; break;
+    case 6: r = 16; break;
+    case 7: r = 17; break;
+    case 8: r = 18; break;
+  }
+  return r;
+}
