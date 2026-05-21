27d7d1987e6b264cc913c86c32f073996965cade
wilkie <wilkie05@gmail.com>
2026-05-20 19:40:57 -0700

    Adds fixtures: signed long mod by pow2 still uses N_LMOD@ (not AND mask); unsigned long >>1 inline as shr/rcr; long * 1L folds to identity.

---
+++ fixtures/1781-long-mod-by-pow2-obj/HELLO.C
@@ -0,0 +1,5 @@
+int main(void) {
+  long a = 100L;
+  long r = a % 4L;
+  return (int)r;
+}
---
+++ fixtures/1782-ulong-shr-by-1-obj/HELLO.C
@@ -0,0 +1,5 @@
+int main(void) {
+  unsigned long a = 100UL;
+  unsigned long r = a >> 1;
+  return (int)r;
+}
