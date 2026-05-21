53224f6c3b535379e822f9523087c22a2d3495f7
wilkie <wilkie05@gmail.com>
2026-05-21 08:35:52 -0700

    Adds fixtures: -O consistent across expr sites (still enregisters); -r- disables register allocation (all locals on stack); -O on recursive factorial removes ~4B.

---
+++ fixtures/2126-O-multi-expr-obj/HELLO.C
@@ -0,0 +1,7 @@
+int main(void) {
+  int x = 5;
+  int y = 10;
+  int sum = x + y;
+  int prod = x * y;
+  return sum + prod;
+}
---
+++ fixtures/2127-r-minus-flag-disable-reg-obj/HELLO.C
@@ -0,0 +1,6 @@
+int main(void) {
+  int x = 5;
+  int y = 10;
+  int z = 20;
+  return x + y + z;
+}
